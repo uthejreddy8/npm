9778df30fc7bb8a999b96af482ed8424e123a87d
uthejreddy8 <107769620+uthejreddy8@users.noreply.github.com>
2023-12-12 14:14:33 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Stop any running services (replace 'your-service' with the actual service name)
-sudo systemctl stop your-service
+#sudo systemctl stop your-service
 
 # Update the application code
 cp -r /home/ubuntu/project/* /home/ubuntu/project/
 
 # Start the services again
-sudo systemctl start your-service
+npm start
+#sudo systemctl start your-service
 
 # Additional deployment steps as needed
